6910e5a4680d0af3c6e656eb2f7ea761d2c9c38a
longnguyen1009 <longbn95@gmail.com>
2021-12-01 15:45:02 +0900

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigurationService } from './configuration/configuration.service';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRoot()],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, ConfigurationService],
+  providers: [AppService],
 })
 export class AppModule {
-  static port: number;
-
-  constructor(
-    private readonly configutationService: ConfigurationService
-  ){
-    AppModule.port = this.configutationService.port as number;
-  }
-
-
 }
