6ced7bfb1bc7602b2b150021a2cdb88b9572a1c3
longnguyen1009 <longbn95@gmail.com>
2021-12-01 18:06:25 +0900

    test create new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { UserModule } from './user/user.module';
       name: "default",
       url: process.env.DATABASE_URL,
       type: 'postgres',
+      port: 5432,
       host: process.env.DATABASE_HOST,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
